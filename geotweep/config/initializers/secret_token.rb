e3282072064c0e662d637be1fc03a9bb4c789c09
tthomander@gmail.com <tthomander@gmail.com>
2014-05-27 19:22:24 -0700

    first commit

---
+++ geotweep/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Geotweep::Application.config.secret_key_base = '8e94dd70e3146d5f93c2b379c4430027474d70ff5373c3c6a4897b1c1293cba6bb58e088a9e828beadb7428d55fff8ba9493f9b626c47fe42eb38e04c383b8ed'
